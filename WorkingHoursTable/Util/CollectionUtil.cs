9903fa19ac8d7c1c4bc7192e8f2080dcf4bb224e
Ruhiel <ruhiel.murrue@gmail.com>
2024-01-07 12:55:22 +0900

    上部コントロール追加

---
+++ WorkingHoursTable/Util/CollectionUtil.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkingHoursTable.Util
+{
+    public static class CollectionUtil
+    {
+        public static void AddAll<T>(this Collection<T> collection, IEnumerable<T> iEnumerable)
+        {
+            foreach (var item in iEnumerable)
+            {
+                collection.Add(item);
+            }
+        }
+    }
+}
--- WorkingHoursTable/ViewModels/MainWindowViewModel.cs
+++ WorkingHoursTable/ViewModels/MainWindowViewModel.cs
@@ -1,11 +1,18 @@
-﻿using System;
+﻿using Reactive.Bindings;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 using WorkingHoursTable.Models;
+using WorkingHoursTable.Util;
+using MahApps.Metro.Controls.Dialogs;
+using System.Diagnostics.Metrics;
+using System.Windows;
+using System.Reactive.Linq;
 
 namespace WorkingHoursTable.ViewModels
 {
@@ -23,16 +30,56 @@ namespace WorkingHoursTable.ViewModels
         }
 
         public ObservableCollection<DateModel> DateList { get; } = new ObservableCollection<DateModel>();
+
+        public ObservableCollection<int> YearList { get; } = new ObservableCollection<int>();
+
+        public ReactiveProperty<int> SelectedYear { get; } = new ReactiveProperty<int>();
+        public ObservableCollection<int> MonthList { get; } = new ObservableCollection<int>();
+
+        public ReactiveProperty<int> SelectedMonth { get; } = new ReactiveProperty<int>();
+        public ReactiveProperty<Visibility> TableVisibility { get; } = new ReactiveProperty<Visibility>();
+        public ReactiveProperty<Visibility> NowProgress { get; } = new ReactiveProperty<Visibility>();
+        public ReactiveCommand PrevMonthCommand { get; } = new ReactiveCommand();
+        public ReactiveCommand NextMonthCommand { get; } = new ReactiveCommand();
         public MainWindowViewModel()
         {
-            SetTable(DateTime.Now);
+            YearList.AddAll(Enumerable.Range(2000, DateTime.Now.Year - 2000 + 1));
+            SelectedYear.Value = DateTime.Now.Year;
+            MonthList.AddAll(Enumerable.Range(1, 12));
+            SelectedMonth.Value = DateTime.Now.Month;
+
+            NowProgress = TableVisibility.Select(x => x == Visibility.Visible ? Visibility.Hidden : Visibility.Visible).ToReactiveProperty();
+
+            SetTable(SelectedYear.Value, SelectedMonth.Value);
+
+            PrevMonthCommand.Subscribe(x =>
+            {
+                var selected = new DateTime(SelectedYear.Value, SelectedMonth.Value, 1);
+                selected = selected.AddMonths(-1);
+                SelectedYear.Value = selected.Year;
+                SelectedMonth.Value = selected.Month;
+                SetTable(SelectedYear.Value, SelectedMonth.Value);
+            });
+
+            NextMonthCommand.Subscribe(x =>
+            {
+                var selected = new DateTime(SelectedYear.Value, SelectedMonth.Value, 1);
+                selected = selected.AddMonths(1);
+                SelectedYear.Value = selected.Year;
+                SelectedMonth.Value = selected.Month;
+                SetTable(SelectedYear.Value, SelectedMonth.Value);
+            });
         }
 
-        private void SetTable(DateTime dateTime)
+        private async void SetTable(int year, int month)
         {
+            DateList.Clear();
+            TableVisibility.Value = Visibility.Hidden;
+
             var collectList = new List<Collect>();
-            var dt = new DateTime(dateTime.Year, dateTime.Month, 1);
-            while (dt.Month == dateTime.Month)
+            var dt = new DateTime(year, month, 1);
+
+            while (dt.Month == month)
             {
                 var collect = new Collect();
 
@@ -44,34 +91,38 @@ namespace WorkingHoursTable.ViewModels
                 collectList.Add(collect);
             }
 
-            // 取得するイベントログ名
-            string logName = "System";
-            // コンピュータ名（"."はローカルコンピュータ）
-            string machineName = ".";
-
-            var idList = new List<long>() { StartEventID, EndEventID };
-
-            // 指定したイベントログが存在しているか調べる
-            if (EventLog.Exists(logName, machineName))
+            await Task.Run(() =>
             {
-                // EventLogオブジェクトを作成する
-                var log = new EventLog(logName, machineName);
+                // 取得するイベントログ名
+                var logName = "System";
+                // コンピュータ名（"."はローカルコンピュータ）
+                var machineName = ".";
 
-                // ログエントリをすべて取得する
-                foreach (var entry in log.Entries.OfType<EventLogEntry>()
-                    .Where(x => x.TimeWritten.Year == dateTime.Year && x.TimeWritten.Month == dateTime.Month && idList.Contains(x.InstanceId)))
+                var idList = new List<long>() { StartEventID, EndEventID };
+
+                // 指定したイベントログが存在しているか調べる
+                if (EventLog.Exists(logName, machineName))
                 {
-                    var first = collectList.Where(x => x.Start <= entry.TimeWritten && entry.TimeWritten <= x.End).FirstOrDefault();
+                    // EventLogオブジェクトを作成する
+                    var log = new EventLog(logName, machineName);
 
-                    if (first != null)
+                    // ログエントリをすべて取得する
+                    foreach (var entry in log.Entries.OfType<EventLogEntry>()
+                        .Where(x => x.TimeWritten.Year == year && x.TimeWritten.Month == month && idList.Contains(x.InstanceId)))
                     {
-                        first.Events.Add(entry);
-                    }
-                }
+                        var first = collectList.Where(x => x.Start <= entry.TimeWritten && entry.TimeWritten <= x.End).FirstOrDefault();
 
-                //閉じる
-                log.Close();
-            }
+                        if (first != null)
+                        {
+                            first.Events.Add(entry);
+                        }
+                    }
+
+                    //閉じる
+                    log.Close();
+                }
+            });
+
 
             foreach (var collect in collectList)
             {
@@ -85,6 +136,8 @@ namespace WorkingHoursTable.ViewModels
 
                 DateList.Add(model);
             }
+
+            TableVisibility.Value = Visibility.Visible;
         }
     }
 }
