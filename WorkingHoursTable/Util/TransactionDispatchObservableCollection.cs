7a34e690962379608c7b0c3bddaf628365e6cf71
Ruhiel <ruhiel.murrue@gmail.com>
2024-01-08 17:54:52 +0900

    警告対応

--- WorkingHoursTable/Util/TransactionDispatchObservableCollection.cs
+++ WorkingHoursTable/Util/TransactionDispatchObservableCollection.cs
@@ -48,47 +48,29 @@ namespace WorkingHoursTable.Util
         /// <summary>
         /// 関連処理を排他制御しながら実行します。(非同期)
         /// </summary>
-        public async Task TransactExcutionAsync(Action action)
-        {
-            await Task.Run(() =>
-            {
-                lock (TransactLock)
-                {
-                    action();
-                }
-            });
-        }
-        protected override void ClearItems()
-        {
-            TransactExcution(() => base.ClearItems());
-        }
-        protected override void InsertItem(int index, T item)
-        {
-            TransactExcution(() => base.InsertItem(index, item));
-        }
-        protected override void MoveItem(int oldIndex, int newIndex)
-        {
-            TransactExcution(() => base.MoveItem(oldIndex, newIndex));
-        }
-        protected override void SetItem(int index, T item)
-        {
-            TransactExcution(() => base.SetItem(index, item));
-        }
-        protected override void RemoveItem(int index)
-        {
-            TransactExcution(() => base.RemoveItem(index));
-        }
+        public async Task TransactExcutionAsync(Action action) => await Task.Run(() =>
+                                                                           {
+                                                                               lock (TransactLock)
+                                                                               {
+                                                                                   action();
+                                                                               }
+                                                                           });
+        protected override void ClearItems() => TransactExcution(() => base.ClearItems());
+        protected override void InsertItem(int index, T item) => TransactExcution(() => base.InsertItem(index, item));
+        protected override void MoveItem(int oldIndex, int newIndex) => TransactExcution(() => base.MoveItem(oldIndex, newIndex));
+        protected override void SetItem(int index, T item) => TransactExcution(() => base.SetItem(index, item));
+        protected override void RemoveItem(int index) => TransactExcution(() => base.RemoveItem(index));
 
         protected override void OnCollectionChanged(NotifyCollectionChangedEventArgs e)
         {
-            if (this.Dispatch.Thread == Thread.CurrentThread)
+            if (Dispatch.Thread == Thread.CurrentThread)
             {
                 base.OnCollectionChanged(e);
             }
             else
             {
                 Action<NotifyCollectionChangedEventArgs> changed = OnCollectionChanged;
-                this.Dispatch.Invoke(changed, Priority, e);
+                Dispatch.Invoke(changed, Priority, e);
             }
         }
     }
